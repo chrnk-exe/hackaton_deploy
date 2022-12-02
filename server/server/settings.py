b122fa7b5dce8fe3c733f694cde77bcce8ebba5e
chrnk-exe <ivan_kot2001@mail.ru>
2022-12-02 19:59:13 +0300

    deploy

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0iwc$j$c!mr#6#b(l6y!1$2!6azzf3g(e!z39zkjsoa2i=#%=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zxchrnk.com']
 
 
 # Application definition
